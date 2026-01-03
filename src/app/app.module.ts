b8431384b62c04b40d1492367318095b332fe080
Omkar982004 <omkarlakhutework2@gmail.com>
2026-01-03 14:42:45 +0530

    completely migrated to module based from standalone component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { App } from "./app";
 import { Header } from "./header/header";
 import { User } from "./user/user";
-import { Tasks } from "./tasks/tasks";
-import { Task } from "./tasks/task/task";
-import { NewTask } from "./tasks/new-task/new-task";
-import { FormsModule } from "@angular/forms";
 import { SharedModule } from "./shared/shared.modules";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
     declarations: [
         App, 
         Header, 
         User, 
-        Task, 
-        Tasks, 
-        NewTask
     ], //non standalone components these modules work with each other
     bootstrap: [App], // root component of the app
     imports: [ 
         BrowserModule, 
-        FormsModule,
-        SharedModule
+        SharedModule,
+        TasksModule
     ] //standalone components
 })
 export class AppModule {}
---
+++ src/app/tasks/tasks.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { Tasks } from "./tasks";
+import { Task } from "./task/task";
+import { NewTask } from "./new-task/new-task";
+import { SharedModule } from "../shared/shared.modules";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+    declarations: [Tasks, Task, NewTask],
+    exports: [Tasks], // we are only using Tasks component outside directly
+    imports: [SharedModule, FormsModule, CommonModule]
+})
+export class TasksModule{
+
+}
